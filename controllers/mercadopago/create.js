b7ce23e5bc6c1598b34975aab5d56b4c7a548026
Rodrigo Cabrera <118491820+Rodrigo-Emca@users.noreply.github.com>
2023-04-18 10:38:54 -0300

    Update create.js

--- controllers/mercadopago/create.js
+++ controllers/mercadopago/create.js
@@ -21,7 +21,7 @@ const controller = {
         quantity: 1,
       })),
       back_urls: {
-        success: "http://localhost:3000/shop",
+        success: "https://mate-ar.vercel.app/shop",
         failure: "",
         pending: "",
       },
